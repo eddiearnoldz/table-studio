df3a51d627ad850a608f5140a87b0c5e141020ff
eddie arnold <eddiearnoldz@mac.com>
2024-08-12 11:29:36 +0100

    removed prismic

--- src/main.js
+++ src/main.js
@@ -2,12 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createPrismic } from "@prismicio/vue";
 import App from './App.vue'
-
-const prismic = createPrismic({
-  endpoint: "tables-london",
-});
 
 const app = createApp(App)
 
